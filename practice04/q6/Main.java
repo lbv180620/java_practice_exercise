cfe3f591fd9ff1e3c5304595761319a75d9dda30
kazuaki_ubuntu210401 <0chlbv7420t18063263g7t20@gmail.com>
2021-06-09 22:28:40 +0900

    210609: 練習問題04設問6の続きをして下さい

--- practice04/q6/Main.java
+++ practice04/q6/Main.java
@@ -4,6 +4,21 @@ import java.io.*;
 
 public class Main {
   public static void main(String[] args) throws IOException {
+    int[] nums = new int[10];
+
     BufferedReader br = new BufferedReader(new InputStreamReader(in));
+    for (int i = 0; i < 10; i++) {
+      out.print("自然数を入力: ");
+      String input = br.readLine();
+      while (!(input.matches("[0-9]|[1-9][0-9]+"))) {
+        out.print("入力し直して下さい: ");
+        input = br.readLine();
+      }
+      nums[i] = Integer.valueOf(input);
+    }
+  }
+
+  public static int sort() {
+    return 0;
   }
 }
